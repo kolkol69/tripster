7a51453075ed036a645b498acb9a909f2e6a4cfc
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-11-14 12:13:39 +0100

    added test project, sys arch, and sequence diagrams

--- src/components/Profile/Post.js
+++ src/components/Profile/Post.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, StyleSheet, Image, Dimensions, } from "react-native";
+import { View, StyleSheet, Image, Dimensions, } from "react-native";
 
 const { height, width } = Dimensions.get('window');
 
