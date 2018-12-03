bde59b17742c6a01ad65663ac0b98443c66cd7c2
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2018-12-03 20:02:25 +0100

    create screen

--- src/screens/CreateArticleScreen.js
+++ src/screens/CreateArticleScreen.js
@@ -1,5 +1,9 @@
+const users = require('../../api/users.json');
+
 import React, { Component } from 'react';
+import { MapView } from 'expo';
 import {
+    Button,
     Platform,
     ScrollView,
     StyleSheet,
@@ -7,6 +11,8 @@ import {
     View,
     StatusBar
 } from 'react-native';
+
+import MapViewDirections from 'react-native-maps-directions';
 
 const instructions = Platform.select({
     ios: 'shake for dev menu',
@@ -18,26 +24,103 @@ export default class CreateArticleScreen extends Component {
         title: 'Create',
     };
 
+    state = {
+        location: {},
+    }
+
+    onPressStart = () => {
+        console.log('zachara START');
+    }
+    onPressAddPOI = () => {
+        console.log('zachara POI');
+    }
+    getCurrPosition = () => {
+        navigator.geolocation.getCurrentPosition((location) => {
+            console.log(location);
+            this.setState({ location });
+        });
+    }
+    componentDidMount() {
+        this.getCurrPosition();
+        console.log('users: ', users[0].tour.places[0].location.lat, users[0].tour.places[0].location.lng);
+        console.log('users typeof: ',typeof users[0].tour.places[0].location.lat, typeof users[0].tour.places[0].location.lng);
+    }
+
     render() {
-        return (
-            <ScrollView >
-                <View style={styles.container}>
-                    <StatusBar
-                        barStyle="light-content"
-                        backgroundColor="#113944"
-                    />
-                    <Text style={styles.welcome}>
-                        Create is commin
-                    </Text>
-                    <Text style={styles.instructions}>
-                        Create is commin
-                    </Text>
-                    <Text style={styles.instructions}>
-                        {instructions}
-                    </Text>
+
+        if (Object.keys(this.state.location).length === 0) {
+            return (
+                <View>
+                    <Text>
+                        Loading..
+                </Text>
                 </View>
-            </ScrollView>
-        );
+            );
+        } else {
+
+            return (
+                <MapView
+                    style={{ flex: 1 }}
+                    initialRegion={{
+                        latitude: this.state.location.coords.latitude,
+                        longitude: this.state.location.coords.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                >
+                    {users[0].tour.places.map((place, i) => {
+                        return (
+                            <MapView.Marker
+                                key={i}
+                                coordinate={{ latitude: place.location.lat, longitude: place.location.lng, }}
+                                title={place.name}
+                                description={place.description}
+                            />
+                        )
+                    })}
+
+                    {/* <MapView.Polyline
+                        coordinates={users[0].tour.places.map((place)=>{
+                            return {
+                                latitude: place.location.lat,
+                                longitude: place.location.lng,
+                            }
+                        })}
+                        strokeWidth={4}
+                    /> */}
+
+                    <MapViewDirections
+                        // origin={{latitude: 50, longitude: 18}}
+                        // destination={{latitude: 51, longitude: 19}}
+                        // origin={{ latitude: 37.3317876, longitude: -122.0054812 }}
+                        // destination={`${users[0].tour.places[2].location.lat}, ${users[0].tour.places[2].location.lng}`}
+
+                        origin={{latitude: users[0].tour.places[0].location.lat, longitude: users[0].tour.places[0].location.lng}}
+                        // waypoints={users[0].tour.places.slice(1,-1).map(el => {return {latitude: el.location.lat, longitude: el.location.lng}})}
+                        destination={{latitude: users[0].tour.places[6].location.lat, longitude: users[0].tour.places[6].location.lng}}
+                        apikey={'AIzaSyADXj2_QGBUUE0p9LCKkFbriU8s2tMBjW4'}
+                    />
+
+                    <View
+                        style={{ bottom: 0 }}
+                    >
+                        <Button
+                            style={{ width: 50, height: 30 }}
+                            onPress={this.onPressStart}
+                            title="Start"
+                            color="#841584"
+                        />
+                        <Button
+                            style={{ width: 50, height: 30 }}
+                            onPress={this.onPressAddPOI}
+                            title="Add POI"
+                            color="#841584"
+                        />
+                    </View>
+                </MapView>
+            );
+        }
+
     }
 }
 
