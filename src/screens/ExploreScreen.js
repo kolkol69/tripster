0beb621f2155e05da7522d46276eaa30d2d30675
Bartosz Wróblewski <6bartek9@gmail.com>
2018-12-17 18:41:08 +0100

    TODO: working list of users tours query list of tours

--- src/screens/ExploreScreen.js
+++ src/screens/ExploreScreen.js
@@ -1,5 +1,34 @@
 import React, { Component } from 'react';
+
+
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyDJ7VPaMgmjFU0bPDnnDW97lvGSwBGPwYI",
+    authDomain: "tripster-5fc5d.firebaseapp.com",
+    databaseURL: "https://tripster-5fc5d.firebaseio.com",
+    projectId: "tripster-5fc5d",
+    storageBucket: "tripster-5fc5d.appspot.com",
+    messagingSenderId: "798900647773"
+};
+
 import {
+    Left,
+    Thumbnail,
+    TextInput,
+    ListItem,
+    Body,
+    List,
+    FlatList,
+    Container,
+    Button,
+    Dimensions,
+    Content,
+    Form,
+    Item,
+    Input,
+    Label,
     Platform,
     ScrollView,
     StyleSheet,
@@ -13,12 +42,148 @@ const instructions = Platform.select({
     android: 'Shake or press menu button for dev menu',
 });
 
+var width = Dimensions.get('window').width; //full width
+
 export default class ExploreScreen extends Component {
     static navigationOptions = {
         title: 'Explore',
     };
 
+    state = {
+        elements: [], //contains elements to display
+        text:'', //text typed in search bar
+        tours: false, //whether or not tours are displayed
+        users: false, //whether or not users are displayed
+    }
+
+    componentDidMount() {
+        console.log("mounted");
+
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
+        firebase.database().ref('/users/users/').once('value').then(resp => console.log(resp.val()));
+    }
+
+    searchForUsers = () => {
+        this.setState({elements: []});
+        console.log(this.state.text);
+        //get users from database and set 'elements' accordingly
+        firebase.database().ref('/users/users/').orderByChild('name').equalTo(this.state.text).once('value')
+            .then(resp => {//this makes it so users are set to be displayed and not tours
+                this.setState({elements: resp.val(),tours: false,users: true});
+                console.log(this.state.elements);
+                console.log(this.state.users);
+            });
+
+
+
+    }
+
+    searchForTours = () => {
+        //get tours from database and set 'elements' accordingly
+        this.setState({tours: true,users: false}); // this makes it so tours are set to be displayed and not users
+    }
+
+    dispSearchForm = () => {
+        return(<TextInput
+                style={{height: 40,  width: width*0.8, left: width*0.1, borderColor: 'gray', borderWidth: 1}}
+                placeholder = ' Type here to search for users or tours '
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}
+            />
+        );
+    }
+
+    dispSearchButtons = () => {
+
+        return (
+
+            <View style={{
+                //zIndex: 1,
+                width: width,
+                top:45,
+                position: 'absolute',
+                flexDirection: 'row',
+                justifyContent: 'space-evenly',
+                alignItems: 'center',
+                alignContent: 'center',
+            }}>
+                <Button
+                    onPress={this.searchForUsers}
+                    title="Users"
+                />
+                <Button
+                    onPress={this.searchForTours}
+                    title="Tours"
+                />
+            </View>
+
+        );
+
+    }
+
+    dispUserList = () => {
+        if(this.state.elements==[] || this.state.elements == null){
+            console.log("sth wrong");
+            return(
+                <Text style = {{
+                    top: 45
+                }}
+                >
+                    No matching profiles
+                </Text>
+            );
+        }
+        else {
+            console.log("returning user list");
+            return (
+                <View style={{
+                    //zIndex: 2,
+                    width: width,
+                    top: 60,
+                    position: 'absolute',
+                }}>
+                    <Container>
+                        <Content>
+                            <List>
+                                <FlatList
+                                    data={this.state.elements}
+                                    renderItem={({item}) => (
+                                        <ListItem
+                                            title={`${item.name}`}
+                                            avatar={{ uri: item.profileImage }}
+                                            subtitle={`${item.region}`}
+                                        />
+                                    )}
+                                />
+                            </List>
+                        </Content>
+                    </Container>
+                </View>
+            );}
+
+    }
+
+    /* keyExtractor={(item, index) => index.toString()}
+
+                                        <Left>
+                                          <Thumbnail square source={{uri: item.result.profileImage}}/>
+                                        </Left>
+                                        <Body>
+                                        <Text>{item.result.name}</Text>
+                                        </Body>
+                                    </ListItem>
+    */
+
     render() {
+        return(<View>
+                {this.dispSearchForm()}
+                {this.dispSearchButtons()}
+                {this.state.users ? this.dispUserList() : null }
+            </View>
+        );
+        /*
         return (
             <ScrollView >
                 <View style={styles.container}>
@@ -36,8 +201,12 @@ export default class ExploreScreen extends Component {
                         {instructions}
                     </Text>
                 </View>
+                <Text>
+                {this.dispSearchForm()}
+                </Text>
             </ScrollView>
         );
+        */
     }
 }
 
@@ -59,4 +228,5 @@ const styles = StyleSheet.create({
         color: '#F5FCFF',
         marginBottom: 5,
     },
+
 });
