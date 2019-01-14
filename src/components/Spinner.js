c9d7eeca6bd2e2698fb2baf8048474bd02df5933
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2019-01-14 18:52:04 +0100

    Add props for customization
    Add props to customize color and size  + prop types for required props

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,24 +1,31 @@
 import React from 'react'
 import { View, ActivityIndicator, StyleSheet } from 'react-native'
+import PropTypes from 'prop-types'
 
-const Spinner = () => {
+
+const Spinner = (props) => {
     return (
         <View style={[styles.container, styles.horizontal]}>
-            <ActivityIndicator size="large" color="#0000ff" />
+            <ActivityIndicator size={props.size} color={props.color} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center'
+        flex: 1,
+        justifyContent: 'center'
     },
     horizontal: {
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      padding: 10
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 10
     }
-  })
+})
+
+Spinner.propTypes = {
+    color: PropTypes.string.isRequired,
+    size: PropTypes.string.isRequired,
+}
 
 export default Spinner
