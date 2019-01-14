abba0667c10dd883aec5b0d0b6dff885d9b22813
maksym kolodiy <maksym.kolodiy.intern@3ds.com>
2019-01-14 21:47:34 +0100

    Remove comments, add route

--- api/server.js
+++ api/server.js
@@ -1,21 +1,14 @@
 const express = require('express');
-// const path = require('path');
-// const open = require('open');
 const start_router = require('./routes/start');
 const fakeUsers = require('./routes/fakeUsers');
 const getPlaces = require('./routes/getPlaces');
 const startTour = require('./routes/startTour');
 const addPOI = require('./routes/addPOI');
 const getUserData = require('./routes/getUserData');
+const updateLikes = require('./routes/updateLikes');
 
 const port = 8873;
 const app = express();
-//https://tripster-5fc5d.firebaseio.com/users/users
-
-
-// const readFromFB = () => {
-//   console.log('usersRef: \n', usersRef)
-// }
 
 app.use((req, res, next) => {
   const origin = req.get('origin');
@@ -31,14 +24,14 @@ app.use('/', start_router);
 app.use('/generateFakeUsers', fakeUsers);
 app.use('/getPlaces', getPlaces);
 app.use('/startTour', startTour);
-app.use('/addPOI/', addPOI);
+app.use('/addPOI', addPOI);
+app.use('/update/likes', updateLikes);
 app.use('/user', getUserData);
 
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
   } else {
-    // open(`http://localhost:${port}`);
     console.log(`Server started at: http://localhost:${port}`)
   }
 });
