08bea08699f28a0e74396d9d56fb0af96660c32a
bwroblewski <6bartek9@gmail.com>
2019-01-15 13:03:30 +0100

    display tours in explore screen
    it works as well as it can with database in it's current form (for example tours lack name parameter so there's no way they can be compared to input text)
    for that reason it could be only tested up to the point where it's checked whether tour name matches input text and up to that point everything works
    TODO:
    1. tours need to be structured according to architecture proposed by Maks (have their name, appropirately structured array 'places' etc)
    2. once that is accomplished it is needed to check whether tours are displayed properly and fix potential errors
    3. finally ability to display user or tour selected from displayed list needs to be added

--- src/screens/ExploreScreen.js
+++ src/screens/ExploreScreen.js
@@ -12,7 +12,7 @@ var firebaseConfig = {
     storageBucket: "tripster-5fc5d.appspot.com",
     messagingSenderId: "798900647773"
 };
-
+import Spinner from '../components/Spinner';
 import {
     TextInput,
     FlatList,
@@ -58,47 +58,83 @@ export default class ExploreScreen extends Component {
 
     state = {
         elements: [], //contains elements to display
-        text:'', //text typed in search bar
+        text: '', //text typed in search bar
         tours: false, //whether or not tours are displayed
         users: false, //whether or not users are displayed
+        loading: false,
     }
 
     componentDidMount() {
-        console.log("mounted");
-
+        //console.log("did mount");
         if (!firebase.apps.length) {
             firebase.initializeApp(firebaseConfig);
         }
-        firebase.database().ref('/users/users/').once('value').then(resp => console.log(resp.val()));
+        // firebase.database().ref('/users/users/').once('value').then(resp => console.log(resp.val()));
     }
 
     searchForUsers = () => {
-        this.setState({elements: []});
-        console.log(this.state.text);
+        this.setState({ elements: [], loading: true });
         //get users from database and set 'elements' accordingly
         firebase.database().ref('/users/users/').orderByChild('name').equalTo(this.state.text).once('value')
             .then(resp => {//this makes it so users are set to be displayed and not tours
-                this.setState({elements: Object.values(resp.val()),tours: false,users: true});
-                console.log(this.state.elements);
-                console.log(this.state.users);
+                // console.log('>>>>>resp', resp.val());
+                this.setState({ elements: Object.values(resp.val()), tours: false, users: true, loading: false });
             });
+    }
 
 
-
+    //checks whether needed tour parameters exist
+    tourCheck = (tour) => {
+        if (tour.places != undefined &&
+            tour.places[0] != undefined &&
+            tour.places[0].image != undefined &&
+            tour.places[0].image.url != undefined &&
+            tour.places[0].image.url != null) return false;
+        else return true;
     }
 
     searchForTours = () => {
-        //get tours from database and set 'elements' accordingly
-        this.setState({tours: true,users: false}); // this makes it so tours are set to be displayed and not users
+        this.setState({ elements: [], loading: true });
+        //get users from database and set 'elements' accordingly
+        firebase.database().ref('/users/users/').once('value') //.orderByChild('tours/name').equalTo(this.state.text).once('value')
+            .then(resp => {
+                //console.log("inside");
+                var toursArray = []; // temporary array for fitting tours
+                if (resp.val() == undefined || resp.val() == null) { //checks whether response contains anything
+                    //console.log("empty");
+                    this.setState({ elements: toursArray, tours: true, users: false, loading: false }); //sets 'elements' array with nothing as nothing has been acquired, sets display parameters to display tours
+                }
+                else {
+                    //console.log("not empty"); 
+                    //console.log("check 1st tour id: ", Object.values(resp.val())[0].tours[0].id);
+                    user_array = Object.values(resp.val());// response value into array
+                    for (let user = 0; user < user_array.length; user++) {
+                        if (user_array[user].tours != null && user_array[user].tours != undefined) {// checks whether user has any tours
+                            tour_array = Object.values(user_array[user].tours);// tours of given user into array
+                            for (let tour = 0; tour < tour_array.length; tour++) {
+                                //console.log('>>>>>resp(tour id):', tour_array[tour].id);
+                                if (checkTour(tour_array[tour]) && tour_array[tour].name == this.state.text) {//checks whether needed parameters of tour exist and whether tour name matches the query
+                                    // pushes object containing user name, tour name, profile image(image of first place) and tour id of matching tour
+                                    toursArray.push({ name: user_array[user].name, tourName: tour_array[tour].name, profileImage: tour_array[tour].places[0].image.url, tourId: tour_array[tour].id });
+                                }
+                            }
+                        }
+                    }
+                    
+                    this.setState({ elements: toursArray, tours: true, users: false, loading: false }); // sets 'elements' array with matching tours, sets display parameters to display tours
+                    //console.log(this.state.elements);
+                }
+                
+            });
     }
 
     dispSearchForm = () => {
-        return(<TextInput
-                style={{height: 40,  width: width*0.8, left: width*0.1, borderColor: 'gray', borderWidth: 1}}
-                placeholder = ' Type here to search for users or tours '
-                onChangeText={(text) => this.setState({text})}
-                value={this.state.text}
-            />
+        return (<TextInput
+            style={{ height: 40, width: width * 0.8, left: width * 0.1, borderColor: 'gray', borderWidth: 1 }}
+            placeholder=' Type here to search for users or tours '
+            onChangeText={(text) => this.setState({ text })}
+            value={this.state.text}
+        />
         );
     }
 
@@ -109,7 +145,7 @@ export default class ExploreScreen extends Component {
             <View style={{
                 //zIndex: 1,
                 width: width,
-                top:45,
+                top: 45,
                 position: 'absolute',
                 flexDirection: 'row',
                 justifyContent: 'space-evenly',
@@ -131,10 +167,10 @@ export default class ExploreScreen extends Component {
     }
 
     dispUserList = () => {
-        if(this.state.elements==[] || this.state.elements == null){
-            console.log("sth wrong");
-            return(
-                <Text style = {{
+        if (this.state.elements == [] || this.state.elements == null) {
+            // console.log("sth wrong");
+            return (
+                <Text style={{
                     top: 45
                 }}
                 >
@@ -143,7 +179,7 @@ export default class ExploreScreen extends Component {
             );
         }
         else {
-            console.log("list");
+            // console.log("list");
             return (
                 <View style={{
                     zIndex: 2,
@@ -156,14 +192,14 @@ export default class ExploreScreen extends Component {
                             <List>
                                 <FlatList
                                     data={this.state.elements}
-                                    renderItem={({item}) => (
+                                    renderItem={({ item }) => (
                                         <ListItem avatar>
                                             <Left>
                                                 <Thumbnail source={{ uri: item.profileImage }} />
                                             </Left>
                                             <Body>
-                                            <Text>{item.name}</Text>
-                                            <Text note>{item.region}</Text>
+                                                <Text>{item.name}</Text>
+                                                <Text note>{item.region}</Text>
                                             </Body>
                                             <Right>
                                                 <Icon name="arrow-forward" />
@@ -175,63 +211,69 @@ export default class ExploreScreen extends Component {
                         </Content>
                     </Container>
                 </View>
-            );}
-
+            );
+        }
     }
 
-    /*
 
 
-
-
-
-     title={`${item.name}`}
-                                        avatar={{ uri: item.profileImage }}
-                                        subtitle={`${item.region}`}
-                                    /
-
-    keyExtractor={(item, index) => index.toString()}
-
-                                        <Left>
-                                          <Thumbnail square source={{uri: item.result.profileImage}}/>
-                                        </Left>
-                                        <Body>
-                                        <Text>{item.result.name}</Text>
-                                        </Body>
-                                    </ListItem>
-    */
+    dispTourList = () => {
+        if (this.state.elements == [] || this.state.elements == null) {
+            // console.log("sth wrong");
+            return (
+                <Text style={{
+                    top: 45
+                }}
+                >
+                    No matching tours
+                </Text>
+            );
+        }
+        else {
+            // console.log("list");
+            return (
+                <View style={{
+                    zIndex: 2,
+                    width: width,
+                    top: 100,
+                    position: 'absolute',
+                }}>
+                    <Container>
+                        <Content>
+                            <List>
+                                <FlatList
+                                    data={this.state.elements}
+                                    renderItem={({ item }) => (
+                                        <ListItem avatar>
+                                            <Left>
+                                                <Thumbnail source={{ uri: item.profileImage }} />
+                                            </Left>
+                                            <Body>
+                                                <Text>{item.tourName}</Text>
+                                                <Text note>{item.name}</Text>
+                                            </Body>
+                                            <Right>
+                                                <Icon name="arrow-forward" />
+                                            </Right>
+                                        </ListItem>
+                                    )}
+                                />
+                            </List>
+                        </Content>
+                    </Container>
+                </View>
+            );
+        }
+    }
 
     render() {
-        return(<View>
-                {this.dispSearchForm()}
-                {this.dispSearchButtons()}
-                {this.state.users ? this.dispUserList() : null }
-            </View>
+        return (<View>
+            {this.dispSearchForm()}
+            {this.dispSearchButtons()}
+            {this.state.users ? this.dispUserList() : this.state.loading && <Spinner />}
+        </View>
         );
-        /*
-        return (
-            <ScrollView >
-                <View style={styles.container}>
-                    <StatusBar
-                        barStyle="light-content"
-                        backgroundColor="#1c5160"
-                    />
-                    <Text style={styles.welcome}>
-                        Explore is commin
-                    </Text>
-                    <Text style={styles.instructions}>
-                        Explore is commin
-                    </Text>
-                    <Text style={styles.instructions}>
-                        {instructions}
-                    </Text>
-                </View>
-                <Text>
-                {this.dispSearchForm()}
-                </Text>
-            </ScrollView>
-        );
-        */
+
     }
 }
 
