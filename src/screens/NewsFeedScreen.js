e1013b53caffb81bc45cad418aae8b5c6aceb829
bwroblewski <6bartek9@gmail.com>
2019-01-19 20:55:21 +0100

    final commit

--- src/screens/NewsFeedScreen.js
+++ src/screens/NewsFeedScreen.js
@@ -8,6 +8,25 @@ import {
     StatusBar
 } from 'react-native';
 
+import * as firebase from 'firebase';
+
+
+import key from '../../api/firebase_apikey.js';
+
+import ProfilePostCards from '../components/Profile/PostCards';
+
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: key,
+    authDomain: "tripster2-0.firebaseapp.com",
+    databaseURL: "https://tripster2-0.firebaseio.com",
+    projectId: "tripster2-0",
+    storageBucket: "tripster2-0.appspot.com",
+    messagingSenderId: "1058324113756"
+
+};
+
+
 const instructions = Platform.select({
     ios: 'shake for dev menu',
     android: 'Shake or press menu button for dev menu',
@@ -18,6 +37,70 @@ export default class NewsFeedScreen extends Component {
         title: 'News Feed',
     };
 
+    state = {
+        userid: 11111,
+        users: [],
+        ready: false
+    }
+
+    getUserPosts = () => {
+        console.log("getting posts");
+        console.log(this.state.userid);
+        this.setState({ posts: [] });
+        firebase.database().ref('/users/users/').orderByChild('id').equalTo(this.state.userid).once('value')
+            .then(resp => {
+                if (resp.val() == undefined || resp.val() == null) { //checks whether response contains anything
+                    this.setState({ users: [] }); //sets 'elements' array with nothing as nothing has been acquired, sets display parameters to display users
+                }
+                else {
+                    var user = resp.val()[0];
+                    console.log(user.following);
+                    var tourArray = [];                
+                    for (let i = 0; i < user.following.length; i++) {
+                        console.log(user.following[i])
+                        firebase.database().ref('/users/users/').orderByChild('id').equalTo(user.following[i]).once('value')
+                            .then(resp => {
+                                console.log("in resp");
+                                //console.log(resp.val());
+                                if (resp.val() == undefined || resp.val() == null) { //checks whether response contains anything
+                                    console.log("no response"); //sets 'elements' array with nothing as nothing has been acquired, sets display parameters to display users
+                                } else {
+                                    var followed = Object.values(resp.val())[0];
+                                    tourArray.push(followed);
+                                    this.setState({ users: tourArray, ready: true });
+                                }
+                            });
+                    }
+                }
+            });
+    }
+
+    componentDidMount = () => {
+        //this.setState({ userid: 11111 });
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
+        this.getUserPosts();
+
+    }
+
+
+    dispPosts = () => {
+        return (this.state.users.map(element => <ProfilePostCards user={element} posts={element.tours} />));
+    }
+
+    render() {
+        return (
+            <ScrollView >
+                <View>
+                    {this.state.ready ? this.dispPosts() : <Text>no posts</Text>}
+                </View>
+            </ScrollView>
+        );
+    }
+}
+
+    /*
     render() {
         return (
             <ScrollView >
@@ -40,6 +123,7 @@ export default class NewsFeedScreen extends Component {
         );
     }
 }
+*/
 
 const styles = StyleSheet.create({
     container: {
